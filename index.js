86a7dae2322f207b9336465f00fe10ae62494ce9
はく <haku@vaguehope.com>
2012-04-06 18:38:37 +0100

    Simplify by removing table, url storage.

--- index.js
+++ index.js
@@ -2,13 +2,10 @@
 
     var input = $("#input textarea");
     var output = $("#output");
-    var annotation = $("#annotation");
 
     function update(event, val) {
         val = val || input.val();
-        annotation.html(tengwar.annotateHtml(val));
         output.html(tengwar.transcribeHtml(val));
-        location.hash = "#" + encodeURIComponent(val);
         input.css({
             // trixsy math coupled too strongly to font
             // metrics on my own machine, but working
